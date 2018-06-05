b6c491e624c551c0357347dd432f19b81cb12833
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-06-05 09:10:13 -0400

    inquirer prompts work and quantity is subtracted properly.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,55 +1,86 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 //establish connection
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.password,
-    database: "bamazon_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.password,
+  database: "bamazon_db"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connection established. " + connection.threadId);
-    promptCustomer();
+  if (err) throw err;
+  console.log("Connection established. " + connection.threadId);
 });
+
+function inventory() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    promptCustomerForItem(res);
+  });
+}
 
 inventory();
 
-    function inventory() {
-    connection.query("SELECT * FROM products", function (err,res) {
-    if (err) throw err;
-    console.table(res);
-    connection.end();
-   
-}
-)};
-
-
-
-
 //starting prompt message to users
-    var promptCustomer = function(res) {
-        inquirer
-        .prompt ([
-            {
-                type: "input",
-                name: "choice",
-                message: "Enter the item_id of the product you would like to buy."
+var promptCustomerForItem = function(inventory) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "Enter the item_id of the product you would like to buy."
+      },
+    ])
+    .then(function(answer) {
+      connection.query("SELECT * FROM products WHERE item_id=?",[answer.choice], function(err,res) {
+        if (err) throw err;
+        var product;
+        for (var i = 0; i < inventory.length; i++) {
+            if (answer.choice==inventory[i].item_id) {
+            product = inventory[i];
             }
-        ]).then (function(answer){
-            var correct = false;
-            for (var i=0; i < res.length; i++) {
-                if(res[i].item_id==answer.choice) {
-                    correct = true;
-                    var product = answer.choice;
-                    var id=i;
-                }
-            }
-          })
-    }
+        }
+        promptCustomerForQuantity(product);
+      });
+    });
+};
 
+var promptCustomerForQuantity = function(product) {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "quantity",
+          message: "How many would you like?"
+        },
+      ])
+      .then(function(answer) {
+          if (answer.quantity < product.stock_quantity) {
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [answer.quantity, product.item_id], function(
+                err,
+                res
+              ) {
+                if (err) throw err;
+                console.log("Successfully Purhcased!");
+                inventory();
+              });
+          }
+        else {
+            console.log("Insufficient Quantity")}
+      });
+  };
+
+
+function makePurchase() {
+
+};
+
+function checkInventory() {
+
+};
